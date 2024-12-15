b648b643cba6908396690cb3e650dfa4ad60affd
maythinn <90564038+developer-maythinn@users.noreply.github.com>
2024-12-15 20:16:27 +0630

    note 2

---
+++ APS/JS/Day2/loop.js
@@ -0,0 +1,16 @@
+// let i = 0;
+// let text = "";
+// while(i < 3){
+//     text += i;
+//     i++;
+// }
+// console.log(text)
+
+let i = 0;
+let text = "";
+do {
+  text += i;
+  i++;
+}
+while (i < 5);
+console.log(text);
