912b7ff90ac491ba65bd00c0503c87cbf1ab6ccb
mtcodingtech <mtcodingtech@gmail.com>
2025-01-30 15:33:13 +0630

    js

---
+++ Aye/JavaScript/Day1/data-types.js
@@ -0,0 +1,10 @@
+// let age = 20.5;
+// console.log(age, typeof age)
+
+
+let username = 'John';
+let message = "How are you?";
+let result = `Hello ${username}, ${message}`;
+
+console.log(result, typeof result)
+console.log("Hello " + username + ", " + message)
---
+++ Aye/JavaScript/Day1/variable.js
@@ -0,0 +1,20 @@
+// var username = "John";
+// username = "Rose";
+
+// let age = 12;
+// age = 20;
+
+// const city = "ygn";
+// city = "Mdy"; // error
+// console.log(username, age, city);
+
+// var username = "John";
+// if (true) {
+//   let username = "Rose";
+//   console.log("let", username);
+// }
+// console.log(username);
+
+"use strict";
+let num = 5;
+console.log(num);
